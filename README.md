c84a194f1b55c19d044423d2be4bef934b8dd048
Jean-Christian Denis <contact@jcdenis.fr>
2025-03-17 20:43:51 +0100

    release 1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an empty theme.
+It is used in Dotclear repository to set default tags.
---
+++ _define.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * @brief       The theme emptyTheme definition
+ * @ingroup     emptyTheme
+ *
+ * @defgroup    emptyTheme Plugin emptyTheme.
+ *
+ * This is an empty theme.
+ *
+ * @author      Jean-Christian Denis (author)
+ * @copyright   AGPL-3.0
+ */
+$this->registerModule(
+    'Empty theme',
+    'This is an empty theme',
+    'Jean-Christian Denis',
+    '1.0',
+    [
+        //'requires'          => [['core', '2.28']],
+        //'standalone_config' => true,
+        'type'              => 'theme',
+        'tplset'            => 'dotty',
+    ]
+);
